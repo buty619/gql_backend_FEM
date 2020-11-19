615a223fffecaf9cfcf586e88035aaa10e01f7d4
cristian <cm-punk-619@hotmail.com>
2020-11-18 20:17:20 -0500

    final version of backend GQL frontend master course

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,35 @@
 
 module.exports = {
   Query: {
-    
+    pets(fromOtherScheema,{input},ctx){
+      return ctx.models.Pet.findMany(input);
+    },
+    pet(fromOtherScheema,{input},ctx){
+      return ctx.models.Pet.findOne(input);
+    },
+    me(fromOtherScheema,{input},ctx){
+      return ctx.models.User.findOne(input);
+    }
   },
   Mutation: {
-    
+    createPet(fromOtherScheema,{input},ctx){
+      const userID = ctx.models.User.findOne().id
+      return ctx.models.Pet.create({...input, userID});
+    }
   },
   Pet: {
-    img(pet) {
+    img(pet,args,ctx) {
       return pet.type === 'DOG'
         ? 'https://placedog.net/300/300'
         : 'http://placekitten.com/300/300'
-    }
+    },
+    user(pet,_,ctx){
+      return ctx.models.User.findOne();
+    },
   },
   User: {
-    
+    pets(user,_,ctx){
+      return ctx.models.Pet.findMany({userID:user.id});
+    },
   }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,49 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  enum animalType {
+    DOG
+    CAT
+  }
 
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: animalType!
+    img: String!
+    userID: ID!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    pets: [Pet!]!
+  }
+
+  input PetInput {
+    name: String!
+    type: animalType
+  }
+
+  input UserName {
+    name: String!
+  }
+
+  type Query {
+    pets(input: PetInput!): [Pet!]!
+    pet(input: PetInput!): Pet!
+    me(input: UserName): User!
+  }
+
+  type Mutation {
+    createPet(input: PetInput!): Pet!
+  }
+
+#   type Mutation {
+    
+#   }
 `;
 
 module.exports = typeDefs
